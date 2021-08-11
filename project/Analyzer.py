37bcac1b6c65cedfa957f72319b787a082eb8161
shaiTheKimhi <shaikimhi221199@gmail.com>
2021-08-12 00:58:02 +0300

    training works on cuda, still does not converge

--- project/Analyzer.py
+++ project/Analyzer.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
-from Attention import AddAttention
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class SentimentAnalyzer(nn.Module):
     def __init__(self, vocab_dim, embedding_dim, h_dim, embedding_weights, layers=1):
@@ -11,22 +12,22 @@ class SentimentAnalyzer(nn.Module):
         self.embd.weight = nn.Parameter(embedding_weights, requires_grad=False)
 
         #LSTM as recurrent layer
-        self.rnn = nn.LSTM(embedding_dim, h_dim, num_layers=layers)
+        self.rnn = nn.RNN(embedding_dim, h_dim, num_layers=layers)
 
         # To convert class scores to log-probability we will add log-softmax layer
         self.log_softmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, X, h_prev=None):
+        self.H = h_dim
+        self.L = layers
+        
+    def forward(self, X):
         # X shape: (S, B) Note:batch dim is not first!
-        
         embedded = self.embd(X) # embedded shape: (S, B, E)
-        B = embedded.shape[1]
-        E = embedded.shape[2]
-        
+        B = torch.tensor([embedded.shape[1]]).to(device)
+        E = torch.tensor([embedded.shape[2]]).to(device)
         # Loop over (batch of) tokens in the sentence(s)
-        ht = None #hidden state
-        ct = None #cell state        
-        for xt in embedded:           # xt is (B, E)
+        ht = torch.zeros(self.L,B,self.H).to(device) #hidden state
+        #ct = torch.zeros(B,self.H).to(device) #cell state        
+        for xt in embedded:           # xt is (B, E) 
             xt = xt.reshape(1,B,E)
             yt, ht = self.rnn(xt, ht) # yt is (B, D_out) #NOTE: we should use cell state for lstm
         
