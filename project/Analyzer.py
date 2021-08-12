35242cb7c5eae2b27d51761d04a49eef547f993e
shaiTheKimhi <shaikimhi221199@gmail.com>
2021-08-12 18:33:23 +0300

    training converge to 70 percent

--- project/Analyzer.py
+++ project/Analyzer.py
@@ -4,21 +4,32 @@ import torch.nn as nn
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class SentimentAnalyzer(nn.Module):
-    def __init__(self, vocab_dim, embedding_dim, h_dim, embedding_weights, layers=1):
+    def __init__(self, vocab_dim, embedding_dim, h_dim, out_dim, embedding_weights, layers=2, bidirec=True):
         super().__init__()
         #embedding from index vector to euclidean based dense vector
         #require_grad set to false for embedding to be fixed and not trained
         self.embd = torch.nn.Embedding(vocab_dim, embedding_dim)
         self.embd.weight = nn.Parameter(embedding_weights, requires_grad=False)
 
-        #LSTM as recurrent layer
-        self.rnn = nn.RNN(embedding_dim, h_dim, num_layers=layers)
-
+        # as recurrent layer
+        self.rnn = nn.GRU(embedding_dim, h_dim, num_layers=layers, bidirectional=bidirec)
+        
+        self.sentiment = nn.Linear(2*h_dim, out_dim)
         # To convert class scores to log-probability we will add log-softmax layer
         self.log_softmax = nn.LogSoftmax(dim=1)
         self.H = h_dim
         self.L = layers
         
+        #self.init_parameters()
+    
+    def init_parameters(self, init_low=-0.15, init_high=0.15):
+        """Initialize parameters. We usually use larger initial values for smaller models.
+        See http://proceedings.mlr.press/v9/glorot10a/glorot10a.pdf for a more
+        in-depth discussion.
+        """
+        for p in self.parameters():
+          p.data.uniform_(init_low, init_high)
+    
     def forward(self, X):
         # X shape: (S, B) Note:batch dim is not first!
         embedded = self.embd(X) # embedded shape: (S, B, E)
@@ -26,13 +37,19 @@ class SentimentAnalyzer(nn.Module):
         E = torch.tensor([embedded.shape[2]]).to(device)
         # Loop over (batch of) tokens in the sentence(s)
         ht = torch.zeros(self.L,B,self.H).to(device) #hidden state
+        ht = None
         #ct = torch.zeros(B,self.H).to(device) #cell state        
         for xt in embedded:           # xt is (B, E) 
             xt = xt.reshape(1,B,E)
-            yt, ht = self.rnn(xt, ht) # yt is (B, D_out) #NOTE: we should use cell state for lstm
+            yt, ht = self.rnn(xt, ht) # yt is (B, H_dim) #NOTE: we should use cell state for lstm
         
         # Class scores to log-probability
         yt = yt.reshape(B, yt.shape[-1])
+        #yt.unsqueeze(0)
+        yt = self.sentiment(yt) #yt is (B,D_out)
+        
         yt_log_proba = self.log_softmax(yt)
         
         return yt_log_proba
+    
+
